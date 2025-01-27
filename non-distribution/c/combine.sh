961a94cd1e498d6c2e238475148b3f011d2a3c48
bkaravan <bohdan_karavan@brown.edu>
2025-01-27 00:56:46 +0000

    working on tests

--- non-distribution/c/combine.sh
+++ non-distribution/c/combine.sh
@@ -9,6 +9,6 @@ p3=$(mktemp -u p3.XXXXXX)
 
 mkfifo "$p1" "$p2" "$p3"
 
-tee >(sort) >(tee "$p1" | tail +2 | paste "$p1" - | sort) >(tee "$p2" | tail +2 | paste "$p2" - | tee "$p3" | cut -f 1 | tail +3 | paste "$p3" - | sort) > /dev/null
+tee >(sort) >(tee "$p1" | tail +2 | paste "$p1" - | sort) >(tee "$p2" | tail +2 | paste "$p2" - | tee "$p3" | cut -f 1 | tail +3 | paste "$p3" - | sort) #> /dev/null
 
 rm "$p1" "$p2" "$p3"
