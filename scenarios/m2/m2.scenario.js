ddea61cda60c9025f5b24018a9b72cd810cdcc09
bkaravan <bohdan_karavan@brown.edu>
2025-02-10 01:45:45 +0000

    scenarios m2

--- scenarios/m2/m2.scenario.js
+++ scenarios/m2/m2.scenario.js
@@ -14,6 +14,9 @@ test('(2 pts) (scenario) simple callback practice', () => {
   }
 
   // ...
+  add(1, 2, storeResults);
+  add(1, 4, storeResults);
+  add(3, 4, storeResults);
 
   expect(results).toEqual([3, 5, 7]);
 });
@@ -26,23 +29,27 @@ test('(2 pts) (scenario) collect errors and successful results', (done) => {
 
   // Sample service
   const appleDeliveryService = (callback) => {
-    // ...
+    callback(false, "good apples");
   };
 
   const pineappleDeliveryService = (callback) => {
     // ...
+    callback(new Error("bad pineapples"), "good pineapples");
   };
 
   const bananaDeliveryService = (callback) => {
+    callback(false, "good bananas");
     // ...
   };
 
   const peachDeliveryService = (callback) => {
-    // ...
+    // ..
+    callback(false, "good peaches");
   };
 
   const mangoDeliveryService = (callback) => {
     // ...
+    callback(new Error("bad mangoes", ""))
   };
 
   const services = [
@@ -95,7 +102,7 @@ test('(5 pts) (scenario) use rpc', (done) => {
 
   const node = {ip: '127.0.0.1', port: 9009};
 
-  let addOneRPC = '?';
+  let addOneRPC = distribution.util.wire.createRPC(distribution.util.wire.toAsync(addOne));
 
   const rpcService = {
     addOne: addOneRPC,
